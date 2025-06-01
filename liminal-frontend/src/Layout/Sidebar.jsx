530a8174148f4bbf555c66091fca24a20db3a6a6
zahid-official <zahid.official8@gmail.com>
2025-06-01 21:22:05 +0600

    Back Home

--- liminal-frontend/src/Layout/Sidebar.jsx
+++ liminal-frontend/src/Layout/Sidebar.jsx
@@ -1,9 +1,9 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import useAuth from "../Auth/Hook/useAuth";
 import profile from "/assets/profile.png";
 import { BsFillGridFill } from "react-icons/bs";
 import { FaDiagramProject } from "react-icons/fa6";
-import { FaChartBar, FaUserCog } from "react-icons/fa";
+import { FaChartBar, FaHome, FaUserCog } from "react-icons/fa";
 
 const Sidebar = () => {
   const { users } = useAuth();
@@ -15,7 +15,7 @@ const Sidebar = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <div className="menu bg-[#f9f9f9] space-y-2 text-base pt-14 shadow-md min-h-screen w-64 p-4">
+        <div className="menu bg-[#f9f9f9] text-base pt-14 shadow-md min-h-screen w-72 p-4">
           {/* profile info */}
           <div>
             <div className="h-16 w-16 mx-auto p-0.5 border-[#154434] border-2 rounded-full">
@@ -25,13 +25,13 @@ const Sidebar = () => {
                 alt=""
               />
             </div>
-            <h2 className="text-2xl font-semibold text-center pt-2 pb-5">
+            <h2 className="text-2xl font-semibold text-center pt-2 pb-6">
               {users?.displayName}
             </h2>
           </div>
 
           {/* navigation */}
-          <ul>
+          <ul className="space-y-2">
             <li>
               <NavLink to="">
                 <BsFillGridFill /> Dashboard
@@ -43,16 +43,25 @@ const Sidebar = () => {
               </NavLink>
             </li>
             <li>
-              <NavLink to="/manageProjects">
+              <NavLink to="manageProjects">
                 <FaChartBar /> Manage Projects
               </NavLink>
             </li>
             <li>
-              <NavLink to="/manageUsers">
+              <NavLink to="manageUsers">
                 <FaUserCog /> Manage Users
               </NavLink>
             </li>
           </ul>
+
+          {/* back home */}
+          <div className="border-t mt-10 pt-2">
+            <li>
+              <Link to="/">
+                <FaHome /> Manage Users
+              </Link>
+            </li>
+          </div>
         </div>
       </div>
     </>
