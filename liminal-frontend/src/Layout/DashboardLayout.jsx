fbd256bc999f4c8650adb3a50bdf07bba519ceb5
zahid-official <zahid.official8@gmail.com>
2025-06-01 20:43:37 +0600

    Profile info

--- liminal-frontend/src/Layout/DashboardLayout.jsx
+++ liminal-frontend/src/Layout/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 const DashboardLayout = () => {
   return (
     <>
-      <div className="drawer">
+      <div className="drawer drawer-open">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* Page content here */}
--- liminal-frontend/src/Layout/Sidebar.jsx
+++ liminal-frontend/src/Layout/Sidebar.jsx
@@ -1,4 +1,8 @@
+import useAuth from "../Auth/Hook/useAuth";
+import profile from "/assets/profile.png";
+
 const Sidebar = () => {
+  const { users } = useAuth();
   return (
     <>
       <div className="drawer-side">
@@ -7,8 +11,22 @@ const Sidebar = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-          {/* Sidebar content here */}
+        <ul className="menu bg-[#f9f9f9] pt-14 shadow-md min-h-screen w-64 p-4">
+          {/* profile info */}
+          <div>
+            <div className="h-16 w-16 mx-auto p-0.5 border-[#154434] border-2 rounded-full">
+              <img
+                src={users?.photoURL || profile}
+                className="w-full h-full object-cover rounded-full"
+                alt=""
+              />
+            </div>
+            <h2 className="text-2xl font-semibold text-center py-2">
+              {users?.displayName}
+            </h2>
+          </div>
+
+          {/* navigation */}
           <li>
             <a>Sidebar Item 1</a>
           </li>
