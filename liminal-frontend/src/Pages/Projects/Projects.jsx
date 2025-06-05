e480e9f4704ec84c164ad1a2b4ba17354852db66
zahid-official <zahid.official8@gmail.com>
2025-06-06 05:29:16 +0600

    Render fetched data using grid layout

--- liminal-frontend/src/Pages/Projects/Projects.jsx
+++ liminal-frontend/src/Pages/Projects/Projects.jsx
@@ -1,18 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Projects = () => {
   const projects = useLoaderData();
   return (
     <div className="max-w-6xl mx-auto px-6 pt-6 pb-32 grid grid-cols-3 gap-x-10 gap-y-28">
       {projects?.map((project) => (
-        <div className="min-h-[57vh]" key={project._id}>
+        <Link to={`/projectDetails/${project._id}`} className="min-h-[57vh] w-full" key={project._id}>
           <img
             src={project.bannerImage}
             className="h-full w-full object-cover"
             alt={`Image of ${project.title}`}
           />
           <h3 className="text-lg pt-3">{project.title}</h3>
-        </div>
+        </Link>
       ))}
     </div>
   );
