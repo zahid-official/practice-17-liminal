d4c1de9ef8317863b72d7fa3a1c0de4ff37e54e0
zahid-official <zahid.official8@gmail.com>
2025-06-11 00:33:22 +0600

    Create API endpoint to delete project

--- liminal-backend/index.js
+++ liminal-backend/index.js
@@ -267,6 +267,16 @@ async function run() {
         res.send(result);
       });
     }
+
+    // delete Operation
+    {
+      app.delete("/deleteProject/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await projectsCollection.deleteOne(query);
+        res.send(result);
+      });
+    }
   } finally {
   }
 }
