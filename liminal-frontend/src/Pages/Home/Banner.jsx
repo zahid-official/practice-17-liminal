a2d21cf9debc2b21a04c89dd4cb0f87b07a65668
zahid-official <zahid.official8@gmail.com>
2025-06-15 03:33:59 +0600

    Updating Design of banner

--- liminal-frontend/src/Pages/Home/Banner.jsx
+++ liminal-frontend/src/Pages/Home/Banner.jsx
@@ -1,18 +1,34 @@
-import banner1 from "/assets/banner1.jpeg";
-import banner2 from "/assets/banner2.jpeg";
-import banner3 from "/assets/banner3.jpeg";
-import banner4 from "/assets/banner4.jpeg";
-import banner5 from "/assets/banner5.jpeg";
-
+import useAxiosPublic from "../../Auth/Hook/useAxiosPublic";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/bundle";
 import { Autoplay, EffectFade } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 const Banner = () => {
+  const axiosPublic = useAxiosPublic();
+
+  // state for latest projects
+  const [latestProjects, setLatestProjects] = useState([]);
+
+  // useEffect for upcoming projects
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axiosPublic.get("/latestProjects");
+        setLatestProjects(res.data);
+      } catch (error) {
+        console.error("Error fetching latest projects:", error);
+      }
+    };
+    fetchData();
+  }, [axiosPublic]);
+
   return (
     <div className="px-6">
-      <h2 className="text-3xl font-semibold custom-title mb-2.5">Latest Projects</h2>
+      <h2 className="text-3xl font-semibold custom-title mb-2.5">
+        Latest Projects
+      </h2>
 
       {/* banner */}
       <Swiper
@@ -25,40 +41,17 @@ const Banner = () => {
         modules={[Autoplay, EffectFade]}
         className="mySwiper max-h-[73vh]"
       >
-        {/* banner-1 */}
-        <SwiperSlide>
-          <div className="flex justify-center">
-            <img src={banner1} className="h-full w-full" alt="" />
-          </div>
-        </SwiperSlide>
-
-        {/* banner-2 */}
-        <SwiperSlide>
-          <div className="flex justify-center">
-            <img src={banner2} className="h-full w-full" alt="" />
-          </div>
-        </SwiperSlide>
-
-        {/* banner-3 */}
-        <SwiperSlide>
-          <div className="flex justify-center">
-            <img src={banner3} className="h-full w-full" alt="" />
-          </div>
-        </SwiperSlide>
-
-        {/* banner-4 */}
-        <SwiperSlide>
-          <div className="flex justify-center">
-            <img src={banner4} className="h-full w-full" alt="" />
-          </div>
-        </SwiperSlide>
-
-        {/* banner-5 */}
-        <SwiperSlide>
-          <div className="flex justify-center">
-            <img src={banner5} className="h-full w-full" alt="" />
-          </div>
-        </SwiperSlide>
+        {latestProjects?.map((project) => (
+          <SwiperSlide key={project?._id}>
+            <div className="flex justify-center">
+              <img
+                src={project?.bannerImage}
+                className="h-full w-full"
+                alt=""
+              />
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
