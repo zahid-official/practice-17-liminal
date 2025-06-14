b1883b93ca43f5276ed804f909202ea5c60c5ec3
zahid-official <zahid.official8@gmail.com>
2025-06-15 03:11:11 +0600

    Updating Design of contact

--- liminal-frontend/src/Pages/Home/Contact.jsx
+++ liminal-frontend/src/Pages/Home/Contact.jsx
@@ -1,13 +1,13 @@
-
-
 const Contact = () => {
-    return (
-        <div className="py-20 text-center bg-base-200 mt-32">
-            <h2 className="text-4xl">Are you ready to Start?</h2>
-            <p className="text-lg py-8">Join us on this creative journey.</p>
-            <button className="btn border-gray-300 rounded-full px-10">Contact</button>
-        </div>
-    );
+  return (
+    <div className="py-20 text-center bg-base-200 lg:mt-40 sm:mt-32 mt-28">
+      <h2 className="text-4xl">Are you ready to Start?</h2>
+      <p className="text-lg py-8">Join us on this creative journey.</p>
+      <button className="btn border-gray-300 rounded-full px-10">
+        Contact
+      </button>
+    </div>
+  );
 };
 
 export default Contact;
