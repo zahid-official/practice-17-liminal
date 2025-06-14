ece6f28c370de1a2e64b0a067bf422817cf149cc
zahid-official <zahid.official8@gmail.com>
2025-06-14 07:28:35 +0600

    Add response interceptor in axios secure

--- liminal-frontend/src/Auth/Hook/useAxiosSecure.jsx
+++ liminal-frontend/src/Auth/Hook/useAxiosSecure.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
   // Add a request interceptor
   axiosSecure.interceptors.request.use(
     function (config) {
@@ -16,6 +21,23 @@ const useAxiosSecure = () => {
       return Promise.reject(error);
     }
   );
+
+  // Add a response interceptor
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      // custom restriction
+      if (error.status === 401 || error.status === 403) {
+        logout()
+          .then(() => navigate("/login"))
+          .catch((err) => console.log(err.message));
+      }
+      return Promise.reject(error);
+    }
+  );
+  
   return axiosSecure;
 };
 
