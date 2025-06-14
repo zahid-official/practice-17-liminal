598f16283fcf44133d073ce0b4e802540f74cc3d
zahid-official <zahid.official8@gmail.com>
2025-06-15 03:08:48 +0600

    Updating routes

--- liminal-frontend/src/Router/Routes.jsx
+++ liminal-frontend/src/Router/Routes.jsx
@@ -5,11 +5,6 @@ import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
-import Upcoming1 from "../Pages/Upcoming/Upcoming1";
-import Upcoming2 from "../Pages/Upcoming/Upcoming2";
-import Upcoming3 from "../Pages/Upcoming/Upcoming3";
-import Upcoming4 from "../Pages/Upcoming/Upcoming4";
-import Upcoming5 from "../Pages/Upcoming/Upcoming5";
 import Projects from "../Pages/Projects/Projects";
 import Contact from "../Pages/Contact/Contact";
 import PrivateRouter from "./PrivateRouter";
@@ -49,26 +44,6 @@ const Routes = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact></Contact>,
-      },
-      {
-        path: "/upcoming1",
-        element: <Upcoming1></Upcoming1>,
-      },
-      {
-        path: "/upcoming2",
-        element: <Upcoming2></Upcoming2>,
-      },
-      {
-        path: "/upcoming3",
-        element: <Upcoming3></Upcoming3>,
-      },
-      {
-        path: "/upcoming4",
-        element: <Upcoming4></Upcoming4>,
-      },
-      {
-        path: "/upcoming5",
-        element: <Upcoming5></Upcoming5>,
       },
       {
         path: "/login",
